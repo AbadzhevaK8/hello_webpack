9953cdbdb1460448ebb0524fdea2e8c9eaba6e90
k8 <make.me.loco@gmail.com>
2022-09-05 14:53:24 +0300

    feat: add config and gitignore.

---
+++ dist/index.317c755020dc302aa2ff.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+ const multiply = (a, b) => a * b;
+ 
+ console.log(multiply(3, 4));
+ console.log(multiply(7, 13));
+/******/ })()
+;
+//# sourceMappingURL=index.317c755020dc302aa2ff.js.map
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import './index.html'; 
+ 
  const multiply = (a, b) => a * b;
  
- console.log(multiply(2, 4));
+ console.log(multiply(3, 4));
+ console.log(multiply(7, 13));
